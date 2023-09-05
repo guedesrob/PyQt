70635ace99365bd4f0ab1e583377c57b31db6d4c
guedesrob <robertolimaguedes@gmail.com>
2023-09-05 15:13:23 -0300

    Delete Readme e add 1a janela

---
+++ aula_1_pyqt.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+# PyQt5.QtWidgets -> Importar submódulo
+# QApplication, QMainWindow -> Importar classes
+
+class Janela (QMainWindow): # Estamos criando uma subclasse Janela, filha da classe QMainWindows
+
+    def __init__(self): # método contrutor
+        super().__init__() # Chama o método construtor da superclasse (QMainWindow) por herança
+        self.topo = 100 # atributo que define a distância do topo da janela à borda da tela superior (100pixel)
+        self.esquerda = 100
+        self.largura = 800
+        self.altura = 600
+        self.titulo = "Primeira Janela"
+        self.CarregarJanela()
+
+    def CarregarJanela(self): # Cria o metodo Carregar janela
+        self.setGeometry(self.esquerda,self.topo,self.largura,self.altura)
+        self.setWindowTitle(self.titulo)
+        self.show()
+
+aplicacao = QApplication(sys.argv) # Cria-se o objeto, referencia do qapplication # sys.argv meche nos paranmetros do sistema, responsavel por fechar a janela
+j= Janela() # Cria-se outro objeto que vai instanciar a classe janela
+sys.exit(aplicacao.exec_())
+# fim
