e75a741fdcc6a1db229edfe81eb341b2eeabf4ca
guedesrob <robertolimaguedes@gmail.com>
2023-09-06 10:15:30 -0300

    Organização dos comentários Aula_1

--- aula_2_pyqt5_botao.py
+++ aula_2_pyqt5_botao.py
@@ -2,17 +2,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip
 
-class Janela (QMainWindow): # Estamos criando uma subclasse Janela, filha da classe QMainWindows
+class Janela (QMainWindow):
 
-    def __init__(self): # método contrutor
-        super().__init__() # Chama o método construtor da superclasse (QMainWindow) por herança
-        self.topo = 100 # atributo que define a distância do topo da janela à borda da tela superior (100pixel)
+    def __init__(self):
+        super().__init__()
+        self.topo = 100
         self.esquerda = 100
         self.largura = 800
         self.altura = 600
         self.titulo = "Primeira Janela"
 
-        botao1 = QPushButton('Botao 1', self) # Cria-se o botao: instatia-se a classe QPushButton
+        botao1 = QPushButton('Botao 1', self) # Cria-se o botao: instacia-se a classe QPushButton
         botao1.move(150,200) # Aplica-se o metodo move ao objeto botao1 (posição do botao )
         botao1.resize(150,80) # Aplica-se o metodo resize ao objeto botao1 (dimensões do botao)
         botao1.setStyleSheet('QPushButton {background-color:#20C12B;font:bold;font-size:20px}') # Usar conversor de rgb para hexadecimal
@@ -32,7 +32,7 @@ class Janela (QMainWindow): # Estamos criando uma subclasse Janela, filha da cla
         self.setWindowTitle(self.titulo)
         self.show()
 
-    def botao1_click(self):
+    def botao1_click(self): # Cria-se um método que printa uma mensagem
         print('O Botao 1 foi clicado')
 
     def botao2_click(self):
@@ -41,4 +41,7 @@ class Janela (QMainWindow): # Estamos criando uma subclasse Janela, filha da cla
 aplicacao = QApplication(sys.argv) # Cria-se o objeto, referencia do qapplication # sys.argv meche nos paranmetros do sistema, responsavel por fechar a janela
 j= Janela() # Cria-se outro objeto que vai instanciar a classe janela
 sys.exit(aplicacao.exec_()) # Não entendi direito o que esse comando faz ()
+# sys.exit(aplicacao.exec_())
+# codigo_de_saida = aplicacao.exec_() #Executa o aplicativo. Para a execução do código, abre a janela, e deixa o usuário interagir com ela. 
+# sys.exit(codigo_de_saida) #fecha o sistema que está sendo executado, retornando o mesmo código de saída do aplicativo.
 # fim
